cee548b04499e2799e65254bbc937977c105909a
Cameron Chandler <webstormcam@gmail.com>
2021-06-01 08:11:27 -0400

    added in a basic ability for variables to be saved in a array

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ const inputBtn=document.getElementById('input-btn')
 
 inputBtn.addEventListener('click',function(){
     console.log('Button clicked from addEventListener')
+    myLeads.push('www.awesomelead.com')
+    console.log(myLeads)
 })
+
