5090b43e3011e76d7816dd9e56377777d20b1582
Cameron Chandler <webstormcam@gmail.com>
2021-06-01 07:44:14 -0400

    Changed the onClick to be only coded in JS
    The onclick was removed from html and purely built in JS

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function saveLead(){
-    console.log('Button clicked!')
-}
+
+
+let inputBtn=document.getElementById('input-btn')
+
+inputBtn.addEventListener('click',function(){
+    console.log('Button clicked from addEventListener')
+})
