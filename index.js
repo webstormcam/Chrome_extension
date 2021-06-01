d4759be295272f74a01cc70fea1ce86947d9cbf6
Cameron Chandler <webstormcam@gmail.com>
2021-06-01 11:30:05 -0400

    adding things to the page with innerHTML

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 
-let myLeads=[]
+let myLeads=['dog','cat','rat']
 const inputEl=document.getElementById('input-el')
 
 const inputBtn=document.getElementById('input-btn')
 
+const ulEl=document.getElementById('ul-el')
+
 inputBtn.addEventListener('click',function(){
     console.log('Button clicked from addEventListener')
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    for(let count=0;count<myLeads.length;count++){
+        ulEl.innerHTML+='<li>'+myLeads[count]+'</li>'
+    }
 })
+
 
