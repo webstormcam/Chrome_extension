da8c6632056a0180ccb48f5578b832e5f7f75851
Cameron Chandler <webstormcam@gmail.com>
2021-06-02 06:30:19 -0400

    improved performance of the site

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
 let myLeads=['dog','cat','rat']
 const inputEl=document.getElementById('input-el')
-
 const inputBtn=document.getElementById('input-btn')
-
 const ulEl=document.getElementById('ul-el')
+let listItems=''
 
 inputBtn.addEventListener('click',function(){
     console.log('Button clicked from addEventListener')
     myLeads.push(inputEl.value)
     for(let count=0;count<myLeads.length;count++){
-        ulEl.innerHTML+='<li>'+myLeads[count]+'</li>'
+        listItems+='<li>'+myLeads[count]+'</li>'
     }
+    ulEl.innerHTML=listItems
 })
 
 
