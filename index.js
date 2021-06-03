471b4da7d8458e400725f5c01b2094250688ff80
Cameron Chandler <webstormcam@gmail.com>
2021-06-03 16:52:06 -0400

    Done
    You can now save links manually, or click the save tab bottom to pull the data from the address bar.

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const inputBtn=document.getElementById('input-btn')
 const ulEl=document.getElementById('ul-el')
 const leadsFromLocalStorage=JSON.parse(localStorage.getItem('myLeads'))
 const deleteBtn=document.getElementById('delete-btn')
+const tabBtn=document.getElementById('tab-btn')
+
+
+
 
 if (leadsFromLocalStorage){
     myLeads=leadsFromLocalStorage
@@ -20,6 +24,16 @@ if (leadsFromLocalStorage){
 })
 
 
+tabBtn.addEventListener('click',function(){
+chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+console.log(tabs)
+myLeads.push(tabs[0].url)
+localStorage.setItem('myLeads',JSON.stringify(myLeads))
+render(myLeads)
+})
+})
+
+
 
 
 
@@ -27,7 +41,7 @@ inputBtn.addEventListener('click',function(){
     myLeads.push(inputEl.value)
     inputEl.value=''
     localStorage.setItem('myLeads',JSON.stringify(myLeads))
-  render(myLeads)
+    render(myLeads)
 })
 
 
