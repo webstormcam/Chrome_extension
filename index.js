6e33b4af5c06af3e47d9b5090c07ba7fb4754250
Cameron Chandler <webstormcam@gmail.com>
2021-06-03 11:43:30 -0400

    Made it so that the links persist across different pages

--- index.js
+++ index.js
@@ -3,23 +3,29 @@ let myLeads=[]
 const inputEl=document.getElementById('input-el')
 const inputBtn=document.getElementById('input-btn')
 const ulEl=document.getElementById('ul-el')
+let leadsFromLocalStorage=JSON.parse(localStorage.getItem('myLeads'))
+
+if (leadsFromLocalStorage){
+    myLeads=leadsFromLocalStorage
+    renderLeads()
+
+}
+
 
 
 
 
 inputBtn.addEventListener('click',function(){
-    console.log('Button clicked from addEventListener')
     myLeads.push(inputEl.value)
     inputEl.value=''
     localStorage.setItem('myLeads',JSON.stringify(myLeads))
-    let leadsFromLocalStorage=JSON.parse(localStorage.getItem('myLeads'))
-
-
-
+  
     renderLeads()
-    console.log(leadsFromLocalStorage)
+
    
 })
+
+
 
 function renderLeads(){
     let listItems=''
