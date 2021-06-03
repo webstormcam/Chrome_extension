9a97570b43356c5c7a601ddd097c41960c161d7f
Cameron Chandler <webstormcam@gmail.com>
2021-06-03 15:03:07 -0400

    finished the delete button

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ let myLeads=[]
 const inputEl=document.getElementById('input-el')
 const inputBtn=document.getElementById('input-btn')
 const ulEl=document.getElementById('ul-el')
-let leadsFromLocalStorage=JSON.parse(localStorage.getItem('myLeads'))
+const leadsFromLocalStorage=JSON.parse(localStorage.getItem('myLeads'))
+const deleteBtn=document.getElementById('delete-btn')
 
 if (leadsFromLocalStorage){
     myLeads=leadsFromLocalStorage
     renderLeads()
 
 }
+
+ deleteBtn.addEventListener('dblclick',function(){
+    localStorage.clear()
+    myLeads=[]
+    ulEl.innerHTML=''
+})
 
 
 
